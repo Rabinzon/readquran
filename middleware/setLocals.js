c02e636df7d23a0b58d58428ae5e5f202744f0ba
Ildar Gilfanov <ildarrazin@gmail.com>
2025-05-03 10:01:14 +0300

    fix locals

--- middleware/setLocals.js
+++ middleware/setLocals.js
@@ -3,6 +3,7 @@ module.exports = (req, res, next) => {
   res.locals.translation = req.translation;
   res.locals.currentYear = new Date().getFullYear();
   res.locals.version = pkg.version;
+  res.locals.isAnaliticsEnabled = req.app.get("env") === "production";
 
   next();
 };
